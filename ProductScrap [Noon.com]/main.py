3809f18cc9c28a791c3470480bb21e72a430ef5c
Mahmudul Hasan <mahmudnotes@outlook.com>
2020-08-13 00:03:19 +0600

    added direct product page scrapping support

--- ProductScrap [Noon.com]/main.py
+++ ProductScrap [Noon.com]/main.py
@@ -4,6 +4,7 @@ import prodscrapper
 import linkScrapper
 from tqdm import tqdm
 
+
 def main():
 
     header = ["ID", "Name", "Rating", "Reviews", "Desc", "Spec", "Overview",
@@ -13,7 +14,7 @@ def main():
                               newline='', encoding="utf-8")
     writer = csv.writer(moviedata_csv_file)
     writer.writerow(header)
-    print("To scrap the entire website, type \'all\' below.")
+    print("To scrap the entire website, type \'all\' below. To scrap a signle product info type \'product\'")
     url = input("Enter a valid listing url: ")
 
     if url != 'all':
@@ -22,8 +23,15 @@ def main():
         urls = linkScrapper.GetProductLinks(url, start, end)
         driver = Firefox()
         for x in urls:
-            print("Processing:",x)
+            print("Processing:", x)
             writer.writerow(prodscrapper.Scrap(x, driver))
+    elif url == 'product':
+        while True:
+            produrl = input("Enter a valid product URL:")
+            writer.writerow(prodscrapper.Scrap(produrl, driver))
+            t = input("Done. Press ENTER to scrap next product. Type exit() to exit the loop.")
+            if t == "exit()":
+                break
     else:
         parentUrl = linkScrapper.GetAllValidProductUrl()
         for u in tqdm(parentUrl):
